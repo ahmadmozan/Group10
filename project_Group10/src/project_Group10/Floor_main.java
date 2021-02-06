65c713efa130846dc57c7d8050bebf0a0d938d15
ahmadmozan <ahmadswayed88@yahoo.com>
2021-02-06 17:25:36 -0500

    Deleted floor, fixed some things

--- project_Group10/src/project_Group10/Floor_main.java
+++ project_Group10/src/project_Group10/Floor_main.java
@@ -1,3 +1,5 @@
+package project_Group10;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -9,18 +11,7 @@ public class Floor_main extends Thread {
 	static String[] output= new String[4];
 	public static Thread floor;
 	
-	public static void main(String[] args) {
-		System.out.println("creating threads...");
-		floor=new Thread();
-		floor.start();
-		getData();
-		String[] op = getArray();
-	    for(String str : op)
-	        System.out.println(str);
-		System.out.println(getArray());
-		getArray();
-		
-	}
+
 	public static void getData() {
 		BufferedReader reader;
 		try {
---
+++ project_Group10/src/project_Group10/Scheduler.java
@@ -0,0 +1,62 @@
+package project_Group10;
+
+public class Scheduler extends Thread{
+	
+	
+	public static String fInput[] = null;
+	
+	public void setfInput(String Inp[]) {
+		
+		while(fInput != null) {
+			try {
+				System.out.println("Input file is currently being processed");
+				wait();
+			}catch (InterruptedException e) {
+				System.out.println(e);
+			}
+		}
+		fInput = Inp;
+		notifyAll();
+		
+	}
+	
+	public void getfInput() {
+		
+		while(fInput == null) {
+			try {
+				System.out.println("Waiting on an input file");
+				wait();
+			}catch (InterruptedException e) {
+				System.out.println(e);
+			}
+		}
+		notifyAll();
+		System.out.println(fInput);
+	}
+	
+	public void notifyElevator() {
+		
+	}
+	
+	public void run(){
+		setfInput(Floor_main.data);
+		String[] op = fInput;
+	    for(String str : op)
+	        System.out.println(str);
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		Thread Floor = new Floor_main();
+		Thread sch = new Scheduler();
+		
+		Floor.run();
+		sch.run();
+		
+		
+	}
+	
+	
+	
+}
