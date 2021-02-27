a7017074d72df84798c0f5df460fbb4a1c4030ec
Akkash Anton Amalarajah <akkash.anton.amalarajah.19@gmail.com>
2021-02-27 18:33:33 -0500

    Adding Button and Updating Floor_main

---
+++ project_Group10/src/project_Group10/Button.java
@@ -0,0 +1,80 @@
+package Group10;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Button {
+	private String direction= null;
+	private int floorNum;
+	private static int max_floors= 5;
+	
+	
+	public synchronized static String direction() {
+		String direction;
+		while (true){
+		
+		Scanner upDown = new Scanner(System.in);
+		System.out.println("Enter: 'Up' or 'Down'.");
+		direction = upDown.nextLine();
+		
+		if (direction.equals("Up")) {
+		System.out.println("Going: "+ direction);
+		break;
+		}
+		else if (direction.equals("Down")) {
+			System.out.println("Going: "+ direction);
+			break;
+		}
+		else {
+			System.out.println("Enter a valid direction: 'Up' or 'Down'");
+		}
+		}
+		return direction;
+	}
+	public static Object getTime() {
+		Date date= new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss.mmm");
+		String dateString=sdf.format(date);
+		System.out.println(dateString);
+		return dateString;
+	}
+	public synchronized static int currentFloor(){
+		int floornum;
+		while (true) {
+		@SuppressWarnings("resource")
+		Scanner floor = new Scanner(System.in);
+		System.out.println("Which floor are you on?");
+		floornum = floor.nextInt();
+		if (floornum<max_floors) {
+			System.out.println("Curent floor: "+floornum);
+			break;
+		}
+		else {
+			System.out.println("There are only 5 floors, which floor are you on?");
+		}
+	}
+		return floornum;
+	}
+	public synchronized static int destFloor() {
+		int floor;
+		while(true) {
+			@SuppressWarnings("resource")
+			Scanner enterFloor = new Scanner(System.in);
+			System.out.println("which floor would you like to go to?");
+			floor=enterFloor.nextInt();
+			if (floor<max_floors) {
+				System.out.println("Going to floor: "+ floor);
+				break;
+			}
+			else {
+				System.out.println("There are only 5 floors");
+			}
+			
+		}
+		return floor;
+		
+	}
+
+}
+
--- project_Group10/src/project_Group10/Floor_main.java
+++ project_Group10/src/project_Group10/Floor_main.java
@@ -1,82 +1,30 @@
-package project_Group10;
-/**
- * This is the Floor subsystem class. It runs the floor subsystem which is responsible for reading the input file and sending the data to the 
- * scheduler for the elevator to use. This class will also be responsible for the floor lamps and buttons, but is not included in this version. This version, v1,
- * shows the communication from the Floor class to the elevator class via the scheduler. 
- * 
- * @author Akkash Anton Amalarajah 101102787
- */
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
+package Group10;
+
 import java.util.Arrays;
-import java.util.Date;
-import java.util.Scanner;
+
 
 public class Floor_main extends Thread {
 	
 	public static int car=1;
-	//public static boolean Inp = false;
-	public Scheduler sch = new Scheduler();
-	public static Object[] input = new Object[4];
+	static Button b;
+	public Scheduler sch =  new Scheduler();
+	
+	static Object[] input = new Object[4];
+	public static void main(String[] args) {
+		inputFile();
+	}
 
 	
-	public synchronized static int currentFloor(){
-		int floornum;
-		while (true) {
-		@SuppressWarnings("resource")
-		Scanner floor = new Scanner(System.in);
-		System.out.println("Which floor are you on?");
-		floornum = floor.nextInt();
-		if (floornum<5) {
-			System.out.println("Curent floor: "+floornum);
-			break;
-		}
-		else {
-			System.out.println("There are only 5 floors, which floor are you on?");
-		}
-	}
-		return floornum;
-	}
-	@SuppressWarnings("resource")
-	public synchronized static String direction() {
-		String direction;
-		while (true){
+	
+	@SuppressWarnings("static-access")
+	public synchronized static Object[] inputFile() {
+		b = new Button();
+		input[0]=b.getTime().toString();
+		input[1]= String.valueOf(b.currentFloor());
+		input[2] = b.direction();
+		input[3]=String.valueOf(car);
 		
-		Scanner upDown = new Scanner(System.in);
-		System.out.println("Enter: 'Up' or 'Down'.");
-		direction = upDown.nextLine();
-		
-		if (direction.equals("Up")) {
-		System.out.println("Going: "+ direction);
-		break;
-		}
-		else if (direction.equals("Down")) {
-			System.out.println("Going: "+ direction);
-			break;
-		}
-		else {
-			System.out.println("Enter a valid direction: 'Up' or 'Down'");
-		}
-		}
-		return direction;
-	}
-	public static Object getTime() {
-		Date date= new Date();
-		SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss.mmm");
-		String dateString=sdf.format(date);
-		System.out.println(dateString);
-		return dateString;
-	}
-	public synchronized Object[] inputFile() {
-		
-		input[0]= getTime().toString();
-		input[1]= String.valueOf(currentFloor());
-		input[2]= direction();
-		input[3]= String.valueOf(car);
-		
-		
+
 		Scheduler.floorno = input[0].toString();
 		Scheduler.Direc = input[1].toString();
 		Scheduler.time = input[2].toString();
@@ -86,7 +34,6 @@ public class Floor_main extends Thread {
 		notifyAll();
 		//Inp = true;
 		return input;
-		
 	}
 	
 	public synchronized Object[] getInput() {
